ed7324c12b6e2765e2258c253f4530875230b6fe
Alex Thomas <hello@cawfree.com>
2020-10-27 02:32:04 +0000

    chore: introduce transform stage

--- example/App.tsx
+++ example/App.tsx
@@ -1,16 +1,13 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { useMerge, Merge } from './lib';
+import { useMerge } from './lib';
 
 export default function App() {
-  console.log('render');
   const { a, b, merged } = useMerge({
     a: useState({ loading: true }),
     b: useState({ loading: false }),
-    //c: { loading: false, string: "true" },
-  });
+  })({});
   console.log({ merged });
-  //console.log({e});
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
--- example/lib/hooks/useMerge.ts
+++ example/lib/hooks/useMerge.ts
@@ -5,6 +5,10 @@ import shouldCombineErrors from "combine-errors";
 import isEqual from "react-fast-compare";
 import debounce from "lodash.debounce";
 
+export type useMergeWithTransformOptions = {
+  [key: string]: unknown[];
+};
+
 export type maybeMergeOptions = {
   [key: string]: unknown[];
 };
@@ -17,11 +21,14 @@ export type useMergeOptions = {
   [key: string]: unknown;
 };
 
+export type useMergeIntermediateResult = (transform: useMergeWithTransformOptions | undefined) => useMergeResult;
+
 export type useMergeResult = {
   [key: string]: unknown;
 };
 
-const shouldMerge = (options: useMergeOptions): useMergeResult => {
+// TODO: Implement transform.
+const shouldMerge = (options: useMergeOptions, transform: useMergeWithTransformOptions): useMergeResult => {
   const merged = Object.entries(options).reduce((obj, [k, v]) => {
     if (v && typeof v === 'object') {
       return Object.entries(v).reduce((obj, [p, v]) => {
@@ -36,20 +43,32 @@ const shouldMerge = (options: useMergeOptions): useMergeResult => {
   return { ...options, merged };
 };
 
-export default function useMerge(options: useMergeOptions): useMergeResult {
+function useMergeWithTransform(
+  options: useMergeOptions,
+  transform: useMergeWithTransformOptions
+): useMergeResult {
   if (!options || typeof options !== 'object') {
     throw new Error(`Expected object options, encountered ${typeof options}.`);
+  } else if (transform !== undefined && typeof transform !== "object") {
+    throw new Error(`Expected object or undefined transform, encountered ${typeof transform}.`);
   }
-  const [merged, setMerged] = useState(() => shouldMerge(options));
+  const [merged, setMerged] = useState(() => shouldMerge(options, transform));
   const [debouncedSetMerged] = useState(() => debounce(
     (e: useMergeResult) => requestAnimationFrame(() => setMerged(e)), 0)
   );
 
   useDeepCompareEffect(() => {
-    const next = shouldMerge(options);
+    const next = shouldMerge(options, transform);
     if (!isEqual(merged, next))
     requestAnimationFrame(() => setMerged(next));
-  }, [options, debouncedSetMerged, shouldMerge]);
+  }, [options, debouncedSetMerged, shouldMerge, transform]);
 
   return merged;
+}
+
+export default function useMerge(options: useMergeOptions): useMergeIntermediateResult {
+  const [mergeWithTransform] = useState(
+    () => (transform: useMergeWithTransformOptions) => useMergeWithTransform(options, transform),
+  );
+  return mergeWithTransform;
 }
